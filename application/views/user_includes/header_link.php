13fe229f86b8aa8d52350583f6048cd55f3c2fec
manju-gilhare <67690422+manju-gilhare@users.noreply.github.com>
2021-02-22 13:52:41 +0530

    Update header_link.php

--- application/views/user_includes/header_link.php
+++ application/views/user_includes/header_link.php
@@ -6,7 +6,7 @@
     <title>CG eDiary - Official Dirary of Chhattisgarh Government</title>
     <meta name=viewport content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv=X-UA-Compatible content="IE=edge" />
-    <link rel="shortcut icon" href="<?=base_url()?>assets/images/favicon.ico">
+    <link rel="shortcut icon" href="<?=base_url()?>assets/images/logo-sm.png">
     <link href="<?=base_url()?>assets/plugins/jvectormap/jquery-jvectormap-2.0.2.css" rel=stylesheet>
     <link href="<?=base_url()?>assets/css/bootstrap.min.css" rel=stylesheet type=text/css />
     <link href="<?=base_url()?>assets/css/icons.min.css" rel=stylesheet type=text/css />
